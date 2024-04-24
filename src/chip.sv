6a09d8654e22f973aa93584d27c17846faf999a5
thomaspila3 <83612628+thomaspila3@users.noreply.github.com>
2024-04-23 22:25:47 -0400

    Update chip.sv with cordic

--- src/chip.sv
+++ src/chip.sv
@@ -7,42 +7,7 @@ module my_chip (
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
     
-    // Basic counter design as an example
-    // TODO: remove the counter design and use this module to insert your own design
-    // DO NOT change the I/O header of this design
-
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
-
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
-
-    always @(posedge clock) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+    tt_um_cordic_wrapper c_wrapper (.clk(clock), .rst(reset), .in_val(io_in[9:0]), .mode_toggle(io_in[10]),
+                                    .out_toggle(io_in[11]), .val(io_out[10:0]), .done(io_out[11]));
 
 endmodule
